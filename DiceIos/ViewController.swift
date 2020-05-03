5d08884565dab4719285035a646d5689531daae2
Riswan Ardiansah <riswanardiansah@gmail.com>
2020-05-04 02:10:17 +0700

    first commit

--- DiceIos/ViewController.swift
+++ DiceIos/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftDiceImageView: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var status: UILabel!
+    
+    
+    var leftDiceNumber = 1
+    var rigthDiceNumber = 2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-
+    
+    
+    @IBAction func onPressRoll(_ sender: UIButton) {
+        
+        var diceArray = [ #imageLiteral(resourceName: "gajah"), #imageLiteral(resourceName: "orang"), #imageLiteral(resourceName: "semut")]
+        
+        leftDiceNumber = Int.random(in: 0...2)
+        rigthDiceNumber = Int.random(in: 0...2)
+        
+        if leftDiceNumber > rigthDiceNumber {
+            status.text? = "player 2 Menang"
+        } else if rigthDiceNumber > leftDiceNumber {
+           status.text? = "player 1 menang"
+        } else {
+            status.text? = "Draaaaaaaw"
+        }
+        
+        leftDiceImageView.image = diceArray[leftDiceNumber]
+        diceImageView2.image = diceArray[rigthDiceNumber]
+        
+       
+    }
+    
 }
 
